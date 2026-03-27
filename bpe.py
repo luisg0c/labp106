560da32748601cb4df14c196390e27646d404b86
luisg0c <lgustavo5555@gmail.com>
2026-03-27 19:47:00 -0300

    merge_vocab

--- bpe.py
+++ bpe.py
@@ -1,4 +1,5 @@
 # BPE: merge par mais frequente, repete K vezes
+import re
 
 vocab = {
     'l o w </w>': 5,
@@ -17,6 +18,16 @@ def get_stats(vocab):
             p = (syms[i], syms[i + 1])
             pairs[p] = pairs.get(p, 0) + 1
     return pairs
+
+
+def merge_vocab(pair, v_in):
+    v_out = {}
+    bigram = re.escape(pair[0]) + r' ' + re.escape(pair[1])
+    pat = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
+    for word, freq in v_in.items():
+        new = pat.sub(pair[0] + pair[1], word)
+        v_out[new] = freq
+    return v_out
 
 
 if __name__ == "__main__":

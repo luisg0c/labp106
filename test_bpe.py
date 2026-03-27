3188c623a80927d4a383c918a34bfa0c5bedd9e5
luisg0c <lgustavo5555@gmail.com>
2026-03-27 20:53:00 -0300

    testa merge

--- test_bpe.py
+++ test_bpe.py
@@ -16,7 +16,16 @@ def test_todos_pares_positivos():
     print("ok: todos pares positivos")
 
 
+def test_merge_junta_par():
+    merged = merge_vocab(('e', 's'), vocab)
+    assert 'n e w es t </w>' in merged, f"merge errado: {list(merged.keys())}"
+    assert 'w i d es t </w>' in merged, f"merge errado: {list(merged.keys())}"
+    assert merged['n e w es t </w>'] == 6, "freq mudou"
+    print("ok: merge junta par")
+
+
 if __name__ == "__main__":
     test_par_mais_frequente()
     test_todos_pares_positivos()
+    test_merge_junta_par()
     print("\ntodos os testes passaram")
